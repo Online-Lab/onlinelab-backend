4fd9cc5918fbaaae53244bd2ea5ad5bb17efc11f
Георгий Дюльдин <g.dyuldin@gmail.com>
2015-09-25 17:43:07 +0300

    Adds image fields upload_to

--- onlinelab/models.py
+++ onlinelab/models.py
@@ -27,13 +27,13 @@ class Project(models.Model):
     name = models.CharField("Название", max_length=100, null=True, blank=True)
     description_short = models.TextField("Краткое описание", null=True, blank=True)
     kind = models.CharField("Тип", max_length=20, choices=TYPE_CHOICES, null=True, blank=True)
-    logo_image = AnyImageField(verbose_name="Логитип проекта", help_text='Для главной', null=True, blank=True)
-    card_image = AnyImageField(verbose_name="Карточка проекта", help_text='Большая', null=True, blank=True)
-    header_image = AnyImageField(verbose_name="Картика в шапку", null=True, blank=True)
+    logo_image = AnyImageField(verbose_name="Логитип проекта", help_text='Для главной', null=True, blank=True, upload_to="projects_images")
+    card_image = AnyImageField(verbose_name="Карточка проекта", help_text='Большая', null=True, blank=True, upload_to="projects_images")
+    header_image = AnyImageField(verbose_name="Картика в шапку", null=True, blank=True, upload_to="projects_images")
     description = models.TextField("Описание", null=True, blank=True)
     side_column = models.TextField("Боковая колонка", null=True, blank=True)
     our_project_image = AnyImageField(
-        verbose_name="Карточка нашего проекта", help_text='Если наш проект', null=True, blank=True)
+        verbose_name="Карточка нашего проекта", help_text='Если наш проект', null=True, blank=True, upload_to="projects_images")
 
     class Meta:
         verbose_name = "Проект"
@@ -49,7 +49,7 @@ class Post(models.Model):
     tags = ArrayField(models.CharField(max_length=200), verbose_name="Теги", blank=True)
     author = models.CharField("Автор", max_length=50, null=True, blank=True)
     date = models.DateField("Дата", default=timezone.now)
-    header_image = AnyImageField(verbose_name="Картинка-шапка", null=True, blank=True)
+    header_image = AnyImageField(verbose_name="Картинка-шапка", null=True, blank=True, upload_to="post_images")
 
     class Meta:
         verbose_name = "Запись"
