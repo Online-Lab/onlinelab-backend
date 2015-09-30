ca3ecf3cd3fa9ef8278cf7f17770d45ee5c58b1f
Георгий Дюльдин <g.dyuldin@gmail.com>
2015-09-30 16:08:32 +0300

    Adds filters

--- onlinelab/views.py
+++ onlinelab/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Project, Post
 from .serializers import ProjectSerializer, PostSerializer
 
@@ -8,8 +8,12 @@ from .serializers import ProjectSerializer, PostSerializer
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('kind',)
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('tags',)
